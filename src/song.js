7c9498d5262824ffc7da6517bff62facbca4938b
Aleksander Repin <alex.repin@mail.ru>
2018-12-29 17:40:50 +0300

    v0.0.12

--- src/song.js
+++ src/song.js
@@ -1,3 +1,14 @@
+const RES_OK = 'OK';
+const ERR_MSG_UNKNOWN = 'Unknown response while parsing song.';
+const FIELD_FILE = 'file';
+const FIELD_LAST_MODIFIED = 'Last-Modified';
+const FIELD_TIME = 'Time';
+const FIELD_ARTIST = 'Artist';
+const FIELD_TITLE = 'Title';
+const FIELD_TRACK = 'Track';
+const FIELD_DATE = 'Date';
+const FIELD_GENRE = 'Genre';
+
 class Song{
 	constructor(info) {
 		if (Array.isArray(info)){
@@ -13,7 +24,7 @@ class Song{
 	flatCopy() {
 		let obj = {};
 		for(let key in this) {
-			if(key !== "mpd"  && this.__proto__[key] === undefined) {
+			if(this.__proto__[key] === undefined) {
 				obj[key] = this[key];
 			}
 		}
@@ -23,40 +34,40 @@ class Song{
 	createFromInfoArray(lines) {
 		let info = {};
 		for(let i = 0; i < lines.length; i++) {
-			let keyValue = lines[i].split(":");
+			let keyValue = lines[i].split(':');
 			if(keyValue.length < 2) {
-				if(lines[i] !== "OK") {
-					throw new Error("Unknown response while parsing song.");
+				if(lines[i] !== RES_OK) {
+					throw new Error(ERR_MSG_UNKNOWN);
 				}
 				else {
 					continue;
 				}
 			}
-			let key = keyValue[0].trim();
-			let value = keyValue[1].trim();
+			let key = keyValue[0].trim(),
+				value = keyValue[1].trim();
 			switch(key) {
-			case "file":
+			case FIELD_FILE:
 				info.file = value;
 				break;
-			case "Last-Modified":
+			case FIELD_LAST_MODIFIED:
 				info.lastModified = new Date(value);
 				break;
-			case "Time":
+			case FIELD_TIME:
 				info.time = value;
 				break;
-			case "Artist":
+			case FIELD_ARTIST:
 				info.artist = value;
 				break;
-			case "Title":
+			case FIELD_TITLE:
 				info.title = value;
 				break;
-			case "Track":
+			case FIELD_TRACK:
 				info.track = value;
 				break;
-			case "Date":
+			case FIELD_DATE:
 				info.date = value;
 				break;
-			case "Genre":
+			case FIELD_GENRE:
 				info.genre = value;
 				break;
 			}
