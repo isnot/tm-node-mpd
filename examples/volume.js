9b12b8fd267df2c0dadff9565a0f6894ab470363
RomanBurunkov <romon2002@gmail.com>
2020-12-07 12:04:22 +0000

    Update volume examples.

--- examples/volume.js
+++ examples/volume.js
@@ -1,15 +1,26 @@
-var MPD = require('../');
-var mpd = new MPD({
-	host : "localhost",
-	port : process.env.MPD_PORT
-});
+const MPD = require('../');
 
-mpd.on("ready", function() {
-	mpd.volume(30, function(err) {
-		console.log(err);
-		mpd.disconnect();
-	});
-});
+const volume = process.argv.length > 2 ? parseInt(process.argv[2], 10) : 30;
+const settings = {
+  host: 'localhost',
+	port: process.env.MPD_PORT || 6600
+};
 
+console.log(`Connecting to the MPD service on ${settings.host}:${settings.port}`);
+const mpd = new MPD(settings);
+
+mpd.on('ready', async () => {
+  try {
+    console.log(`MPD connection is ready, protocol ver ${mpd.server.version}.`)
+    console.log(`Sending volume ${volume} command...`);
+    await mpd.volume(volume);
+    console.log(`Command volume ${volume} has been successfully sent.`);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    console.log(`Disconnecting from ${settings.host}:${settings.port}`);
+    mpd.disconnect();
+  }
+});
 
 mpd.connect();
