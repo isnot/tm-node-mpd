a3f5bc3c178795db2bbaa6f188e113520344d186
RomanBurunkov <romon2002@gmail.com>
2020-12-16 17:59:18 +0000

    Add parseGreeting func and tests for it

--- src/protocol.js
+++ src/protocol.js
@@ -9,7 +9,22 @@
 module.exports.parseKvp = (kvp = '') => {
   if (!kvp) return false;
   const m = kvp.match(/(\S+)\s*:\s*(\S+)/);
-  return !Array.isArray(m) || m.length !== 3
-    ? false
-    : { key: m[1].trim(), val: m[2].trim() };
+  return Array.isArray(m) && m.length === 3
+    ? { key: m[1].trim(), val: m[2].trim() }
+    : false;
+};
+
+/**
+ * Parses MPD Greeting message
+ * According to the MPD protocol documentation when the client connects to the server,
+ * the server will answer with the following line: 'OK MPD version'
+ * where version is a protocol version identifier such as 0.12.2.
+ * @param {string} message MPD greeting message.
+ * @returns {Object} mpd protocol details: { name: service name(MPD), version: protocol version}.
+ */
+module.exports.parseGreeting = (message = '') => {
+  const m = message.match(/OK\s(.+)\s(.+)/);
+  return Array.isArray(m) && m.length === 3
+    ? { name: m[1], version: m[2] }
+    : false;
 };
--- tests/protocol.test.js
+++ tests/protocol.test.js
@@ -1,5 +1,49 @@
-const { parseKvp } = require('../src/protocol');
+const proto = require('../src/protocol');
 
-test('parseKvp should return false if no data passed', () => {
-  expect(parseKvp()).toBe(false);
+describe('Test parseKvp function', () => {
+  test('Is defined', () => expect(proto.parseKvp).toBeDefined());
+  test('Returns false if no data passed', () => expect(proto.parseKvp()).toBe(false));
+  test('Returns false if passed string doesnt match kvp format', () => {
+    expect(proto.parseKvp('no kvp')).toBe(false);
+  });
+  test('Doesnt returns false if passed string matches kvp format', () => {
+    expect(proto.parseKvp('vol: 42')).toBeTruthy();
+  });
+  test('Returned value has key prop if passed string matches kvp format', () => {
+    const kvp = proto.parseKvp('vol: 42');
+    expect(kvp.key).toBeDefined();
+  });
+  test('Returned value has val prop if passed string matches kvp format', () => {
+    const kvp = proto.parseKvp('vol: 42');
+    expect(kvp.val).toBeDefined();
+  });
+  test('Returns a kvp object if passed string matches kvp format', () => {
+    const kvp = proto.parseKvp('vol: 42');
+    expect(kvp.key).toBe('vol');
+    expect(kvp.val).toBe('42');
+  });
+});
+
+describe('Test parseGreeting function', () => {
+  test('Is defined', () => expect(proto.parseGreeting).toBeDefined());
+  test('Returns false if no data passed', () => expect(proto.parseGreeting()).toBe(false));
+  test('Returns false if passed string doesnt match greetings format', () => {
+    expect(proto.parseGreeting('Failed greetings')).toBe(false);
+  });
+  test('Doesnt returns false if passed string matches greetings format', () => {
+    expect(proto.parseGreeting('OK MPD 0.20.2')).toBeTruthy();
+  });
+  test('Returned value has name prop if passed string matches greetings format', () => {
+    const greeting = proto.parseGreeting('OK MPD 0.20.2');
+    expect(greeting.name).toBeDefined();
+  });
+  test('Returned value has version prop if passed string matches greetings format', () => {
+    const greeting = proto.parseGreeting('OK MPD 0.20.2');
+    expect(greeting.version).toBeDefined();
+  });
+  test('Returns a protocol info object if passed string matches greetings format', () => {
+    const greeting = proto.parseGreeting('OK MPD 0.20.2');
+    expect(greeting.name).toBe('MPD');
+    expect(greeting.version).toBe('0.20.2');
+  });
 });
